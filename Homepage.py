8ee7edc625a479fefe7890734b5c364149a9eef6
Ojo Oreoluwa <98988569+Manuelorejo@users.noreply.github.com>
2025-02-25 00:30:55 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page configuration
 st.set_page_config(
-    page_title="Tech Career Advisor",
+    page_title="Career Path Predictor",
     page_icon="🧭",
     layout="wide"
 )
